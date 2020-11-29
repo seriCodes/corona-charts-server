c874ef5519429e7ef38f40291e3fe2ce7be901c3
Saar Seri <revatemany@hotmail.com>
2020-11-29 07:01:46 +0200

    app.get change

--- server/app.js
+++ server/app.js
@@ -34,7 +34,9 @@ app.use(chartRouter);
 // app.get('/ping', function (req, res) {
 //     return res.send('pong');
 // });
-app.get('/*', function (req, res) {
+
+// app.get('/*', function (req, res) {
+  app.get('*', function (req, res) {
     res.sendFile(path.join(buildPath, 'index.html'));
 });
 
